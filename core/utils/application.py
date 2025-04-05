2d5558b8acf2587992199ec9da928805d19fca5e
Oğuzhan Çınar <ogzhn.cnr0@gmail.com>
2025-04-06 01:00:29 +0300

    Update application.py

--- core/utils/application.py
+++ core/utils/application.py
@@ -9,7 +9,6 @@ from core.search.service.mutator.one_zero_mutator import OneZeroMutator
 from core.search.service.mutator.standard_mutator import StandardMutator
 from core.search.service.pruner.standard_pruner import StandardPruner
 from core.search.service.sampler.random_sampler import RandomSampler
-#from core.search.service.fitness_function.untargeted_fitness_function import TargetedFitnessFunction
 from core.search.service.fitness_function.untargeted_fitness_function import UntargetedFitnessFunction
 
 
@@ -44,8 +43,7 @@ def configure_container(container):
         container.ff.override(providers.Singleton(UntargetedFitnessFunction,
                                                   archive=container.archive,
                                                   remote_controller=container.remote_controller,
-                                                  stc=container.stc,
-                                                  target_label=container.config.get("target")))
+                                                  stc=container.stc))
     else:
         container.ff.override(providers.Singleton(UntargetedFitnessFunction,
                                                   archive=container.archive,
