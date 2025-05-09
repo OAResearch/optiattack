617e27ea3a87eb9fa3c826363642e8d30937547f
Omur <omur@erciyes.edu.tr>
2025-05-09 12:23:34 +0200

    client version v0.0.2

--- client/setup.py
+++ client/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="optiattack_client",
-    version="0.1.1",
+    version="0.1.2",
     description="A FastAPI decorator to track method calls and manage state.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
